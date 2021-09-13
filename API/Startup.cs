cbbc73d6833f02db9a16554ad3f1d30d54a8c21e
Stephen Ball <stephen_ball1993@outlook.com>
2021-09-13 13:15:58 +1000

    Cors Policy & Semantic-UI added

--- API/Startup.cs
+++ API/Startup.cs
@@ -26,7 +26,7 @@ namespace API
        
         }
 
-   
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
@@ -43,7 +43,14 @@ namespace API
             {
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
-
+            //Add cors policy
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -60,12 +67,14 @@ namespace API
 
             app.UseRouting();
 
+            app.UseCors("CorsPolicy");
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
-        }
+        }       
     }
 }
