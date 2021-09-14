4ede9131d040fffca570ce67c5141e99d60548c6
Stephen Ball <stephen_ball1993@outlook.com>
2021-09-14 19:39:37 +1000

    Added Mediator functionality for GET requests

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
 
 
 namespace API.Controllers
@@ -12,8 +9,12 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
     {
-       
-        
+        private IMediator _mediator;
+
+        //Initialise Mediator field if null value 
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices
+            .GetService<IMediator>();
+
     }
 
 }
