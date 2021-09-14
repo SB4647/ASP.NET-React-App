f4b06579efec5fc5a38815366d2a2f22bcfc7d6c
Stephen Ball <stephen_ball1993@outlook.com>
2021-09-14 20:40:23 +1000

    create activity endpoint implemeted

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -11,18 +11,26 @@ namespace API.Controllers
 
     public class ActivitiesController : BaseApiController
     {
-   
-        
-      [HttpGet]
-      public async Task<ActionResult<List<Activity>>> GetActivities()
+
+        //Get all activities
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
         {
             return await Mediator.Send(new List.Query());
         }
 
-      [HttpGet("{id}")]
-      public async Task<ActionResult<Activity>> GetActvity(Guid id)
+        //Get an activity based on id 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActvity(Guid id)
         {
             return await Mediator.Send(new Details.Query{ Id = id });
+        }
+
+        //Create an activity sent in body
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity activity)
+        {
+            return Ok(await Mediator.Send(new Create.Command { Activity = activity }));
         }
 
     }
