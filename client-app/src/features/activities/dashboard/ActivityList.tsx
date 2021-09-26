ffcc3f6dd4e9c0be6fd090ae1960fe53c2939583
Stephen Ball <stephen_ball1993@outlook.com>
2021-09-26 16:50:13 +1000

    Integration of MobX state management

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,20 +1,12 @@
 import React, { SyntheticEvent, useState } from "react";
+import { observer } from "mobx-react-lite";
 import { Button, Item, Label, Segment } from "semantic-ui-react";
-import { Activity } from "../../../app/models/activity";
+import { useStore } from "../../../app/stores/store";
 
-interface Props {
-    activities: Activity[];
-    selectActivity: (id: string) => void;
-    deleteActivity: (id: string) => void;
-    submitting: boolean;
-}
+export default observer(function ActivityList() {
+    const { activityStore } = useStore();
+    const { deleteActivity, activities, loading } = activityStore;
 
-export default function ActivityList({
-    activities,
-    selectActivity,
-    deleteActivity,
-    submitting,
-}: Props) {
     const [target, setTarget] = useState("");
 
     function handleActivityDelete(
@@ -40,16 +32,18 @@ export default function ActivityList({
                             </Item.Description>
                             <Item.Extra>
                                 <Button
-                                    onClick={() => selectActivity(activity.id)}
+                                    onClick={() =>
+                                        activityStore.selectActivity(
+                                            activity.id
+                                        )
+                                    }
                                     floated="right"
                                     content="View"
                                     color="blue"
                                 />
                                 <Button
                                     name={activity.id}
-                                    loading={
-                                        submitting && target === activity.id
-                                    }
+                                    loading={loading && target === activity.id}
                                     onClick={e =>
                                         handleActivityDelete(e, activity.id)
                                     }
@@ -65,4 +59,4 @@ export default function ActivityList({
             </Item.Group>
         </Segment>
     );
-}
+});
