ce4525324c52c859dcf0ebef0dcfc6758efd409b
Stephen Ball <stephen_ball1993@outlook.com>
2021-10-01 17:01:11 +1000

    Error handling set-up

---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,57 @@
+﻿using Application.Core;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace API.Middleware
+{
+
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        private readonly IHostEnvironment _env;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+        {
+            _next = next;
+            _logger = logger;
+            _env = env;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex) 
+            {
+                //If Http request fails to pass through middleware then send a 500 response back
+
+                _logger.LogError(ex, ex.Message);
+                context.Response.ContentType = "application/json";
+                //set status code to 500
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                //If in development mode send stacktrace & error message back, if in production mode send basic message
+                var response = _env.IsDevelopment()
+                    ? new AppException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString()) 
+                    : new AppException(context.Response.StatusCode, "Server Error");
+
+                //Ensure that response is in camel case format
+                var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+                var json = JsonSerializer.Serialize(response, options);
+
+                await context.Response.WriteAsync(json);
+            }
+        }
+
+    }
+
+}
