fe4c7aea6c40acd7494eadb2d1f48d7721e52e09
Stephen Ball <stephen_ball1993@outlook.com>
2021-10-17 20:36:38 +1000

    Integrated Formik form library

--- client-app/src/app/stores/commonStore.ts
+++ client-app/src/app/stores/commonStore.ts
@@ -1,4 +1,4 @@
-import { ServerError } from "../models/ServerError";
+import { ServerError } from "../models/serverError";
 import { makeAutoObservable } from "mobx";
 
 export default class CommonStore {
